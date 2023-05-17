39f09907fcf6f1a10f8e5fc061f2dc07e36396f6
Terry#1234 <68700354+teraprath@users.noreply.github.com>
2023-05-17 21:00:05 +0200

    2.0.0-RELEASE
    + Update CommandBuilder
    + Update SQLAdapter and SQLAuthentication class
    + Added new SQLTableBuilder Class for quicker Table creation
    + Removed ScoreboardBzuilder & ItemManager

---
+++ src/main/java/dev/teraprath/corelib/sql/SQLAuthentication.java
@@ -0,0 +1,12 @@
+package dev.teraprath.corelib.sql;
+
+public class SQLAuthentication {
+
+    public String HOST = "localhost";
+    public int PORT = 3306;
+    public String USER = "root";
+    public String PASSWORD = "password";
+    public String DATABASE = "database";
+
+}
+
